eae51c6396ceda2403473ed5e94b442ac87926cf
yotam hochman <yotamho@gmail.com>
2024-11-21 16:27:06 +0200

    implement gallery

--- lib/src/delegates/gallery_builder_delegate.dart
+++ lib/src/delegates/gallery_builder_delegate.dart
@@ -3,8 +3,10 @@ import 'package:wechat_assets_picker/src/delegates/gallery_viewer_builder_delega
 import 'package:wechat_assets_picker/wechat_assets_picker.dart';
 
 class GalleryBuilderDelegate extends DefaultAssetPickerBuilderDelegate {
-  GalleryBuilderDelegate({required super.provider, required super.initialPermission, super.locale});
+  GalleryBuilderDelegate({required super.provider, required super.initialPermission, super.locale, this.customGridItemWidgetBuilder, this.viewerBottomDetailWidgetBuilder});
 
+  final Widget Function(BuildContext, int, AssetEntity)? customGridItemWidgetBuilder;
+  final Widget Function(BuildContext, int, AssetEntity)? viewerBottomDetailWidgetBuilder;
 
   @override
   Future<void> viewAsset(BuildContext context, int? index, AssetEntity currentAsset) async {
@@ -13,7 +15,20 @@ class GalleryBuilderDelegate extends DefaultAssetPickerBuilderDelegate {
     }
     await AssetPickerViewer.pushToViewerWithDelegate(
       context,
-      delegate: GalleryViewerBuilderDelegate(currentIndex: index, previewAssets: [currentAsset], themeData: theme),
+      delegate: GalleryViewerBuilderDelegate(currentIndex: index, previewAssets: [currentAsset], themeData: theme, bottomDetailWidgetBuilder: viewerBottomDetailWidgetBuilder),
     );
   }
+
+  @override
+  Widget bottomActionBar(BuildContext context) => const SizedBox.shrink();
+
+  @override
+  Widget selectIndicator(BuildContext context, int index, AssetEntity asset) {
+    if (customGridItemWidgetBuilder != null) {
+      return customGridItemWidgetBuilder!(context, index, asset);
+    } else {
+      return const SizedBox.shrink();
+    }
+  }
+
 }
