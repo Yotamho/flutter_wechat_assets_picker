5942b534480274d01771da936985911789be3b76
yotam hochman <yotamho@gmail.com>
2024-11-21 16:30:46 +0200

    add context actions to GalleryBuilderDelegate

--- lib/src/delegates/gallery_builder_delegate.dart
+++ lib/src/delegates/gallery_builder_delegate.dart
@@ -3,7 +3,7 @@ import 'package:wechat_assets_picker/src/delegates/gallery_viewer_builder_delega
 import 'package:wechat_assets_picker/wechat_assets_picker.dart';
 
 class GalleryBuilderDelegate extends DefaultAssetPickerBuilderDelegate {
-  GalleryBuilderDelegate({required super.provider, required super.initialPermission, super.locale, this.customGridItemWidgetBuilder, this.viewerBottomDetailWidgetBuilder});
+  GalleryBuilderDelegate({required super.provider, required super.initialPermission, super.locale, super.contextActions, this.customGridItemWidgetBuilder, this.viewerBottomDetailWidgetBuilder});
 
   final Widget Function(BuildContext, int, AssetEntity)? customGridItemWidgetBuilder;
   final Widget Function(BuildContext, int, AssetEntity)? viewerBottomDetailWidgetBuilder;
